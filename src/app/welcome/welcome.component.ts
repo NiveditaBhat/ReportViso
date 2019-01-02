efa627b7ab8c34708e3e3ecdc6c4ff4fadb5f552
Nivedita <niveditabhat92@gmail.com>
2019-01-02 17:05:17 +0100

    welcome page routing with fragment done

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router , NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-welcome',
@@ -10,7 +10,17 @@ export class WelcomeComponent implements OnInit {
 
   constructor(private router: Router) { }
 
+
   ngOnInit() {
+    this.router.events.subscribe(s => {
+      if (s instanceof NavigationEnd) {
+        const tree = this.router.parseUrl(this.router.url);
+        if (tree.fragment) {
+          const element = document.querySelector('#' + tree.fragment);
+          if (element) { element.scrollIntoView(); }
+        }
+      }
+    });
   }
 
 signupForm() {
